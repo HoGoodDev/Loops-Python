7150d6754483ea48fd75b986c3a2e0f1422bb23e
HoGoodDev <hogood.dev@gmail.com>
2025-07-04 20:07:18 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# secret_num = 72
+# guess_count = 0
+# guess_limit = 3
+
+# while guess_count < guess_limit:
+#   guess = int(input("Guess: "))
+#   guess_count += 1
+#   if guess == secret_num:
+#     print("you won!!")
+#     break
+# else: 
+#   print("u lost!!!")
+
+
+
